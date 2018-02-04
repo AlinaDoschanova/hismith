0256607b36abd929bb54976f812de045365d7fac
Alina Doschanova <doschanova.alina@gmail.com>
2018-02-04 17:24:43 +0600

    Старт

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+svg4everybody(); // иницализация полифила для IE
+
+$(document).ready(function(){
+
+  // Включение слайдера
+  $('#diagram-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    items:1,
+  });
+
+});
+
+// Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
+// function ready(fn) {
+//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+//
+// ready(function(){
+//   // code
+// });
